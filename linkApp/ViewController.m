573d54b00cc9cf6a7372ae3734a84d935ae26369
Bernard Ferguson <nardy_reborn@Bernard-Fergusons-MacBook-Pro.local>
2013-05-01 22:58:21 -0500

    commited changes from last week meeting

--- linkApp/ViewController.m
+++ linkApp/ViewController.m
@@ -27,6 +27,10 @@
         UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
         [button setTitle:[self.times objectAtIndex:i] forState:UIControlStateNormal];
          button.frame     = CGRectMake(30, y, 100,25  );
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                    action:@selector(createnotification:)];
+        
+        [button addGestureRecognizer:tap];
         [ self.scrollView addSubview:button];
         y+= 50;
         self.scrollView.contentSize = CGSizeMake(200, y);
@@ -40,5 +44,52 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(void)createnotification:(UIGestureRecognizer*)gestureRecognizer  //call this function to produce a local notification even if your app is running in background.
 
+{
+    NSString *dateandtime = @"17:24";//((UIButton*)gestureRecognizer.view).titleLabel.text;
+    NSLog(@"%@", dateandtime);
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc]init];
+    
+    [formatter setDateFormat:@"HH:mm"];
+    
+    //[formatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:-18000]];
+    
+    NSDate *alerttime = [formatter dateFromString:dateandtime];
+    
+    UIApplication * app = [UIApplication sharedApplication];
+    
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    
+    if (notification)
+        
+    {
+        
+        notification.fireDate = alerttime;
+        
+        //notification.timeZone = [NSTimeZone defaultTimeZone];
+        
+        notification.repeatInterval = 0;
+        
+       // notification.alertBody = message;
+        
+        [app scheduleLocalNotification:notification];
+        
+        [app presentLocalNotificationNow:notification];
+        NSLog(@"SET");
+        
+    }
+    
+}
+
+//the notifications will go to the function given below if your app is running in forground
+
+-(void)application:(UIApplication *)application didRecieveLocalnotification:(UILocalNotification *)notification
+
+{
+    
+    NSLog(@"Notification Received");
+    
+}
 @end
